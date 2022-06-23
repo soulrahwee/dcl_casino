eeff228eb5773119c7660461cec271638436e9ab
somalyleuk <malyonly88@gmail.com>
2022-06-23 15:14:27 +0700

    cc

--- src/stream/stream2.ts
+++ src/stream/stream2.ts
@@ -5,17 +5,17 @@ stream2.addComponent(new Transform({ scale: new Vector3(1, 1, 0) }))
 engine.addEntity(stream2)
 
 const screenStream2 = new Entity()
-screenStream2.addComponent(new Transform({ position: new Vector3(-10, 0.99, 40), rotation: Quaternion.Euler(0, 90, 0) }))
+screenStream2.addComponent(new Transform({ position: new Vector3(-27, 2, 44), rotation: Quaternion.Euler(0, 90, 0) }))
 engine.addEntity(screenStream2)
 
 const screenTransform2 = new Entity()
-screenTransform2.addComponent(new Transform({ position: new Vector3(0, 0.99, 0) }))
+screenTransform2.addComponent(new Transform({ position: new Vector3(0, 2, 0) }))
 screenTransform2.getComponent(Transform).rotate(Vector3.Right(), 0)
 screenTransform2.setParent(screenStream2)
 
 const screen2 = new Entity()
 screen2.addComponent(new PlaneShape())
-screen2.addComponent(new Transform({ scale: new Vector3(5.45, 2.90, 2.90) }))
+screen2.addComponent(new Transform({ scale: new Vector3(13.60, 6, 6) }))
 screen2.getComponent(Transform).rotate(Vector3.Up(), 0)
 screen2.setParent(screenTransform2)
 screenTransform2.getComponent(Transform).scale.setAll(0.6) // You can change the scale of the screen here...
