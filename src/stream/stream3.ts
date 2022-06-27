44667f20015d6e8fdf4a6ba9f2fb4f54f774720c
somalyleuk <malyonly88@gmail.com>
2022-06-27 09:22:10 +0700

    dd

---
+++ src/stream/stream3.ts
@@ -0,0 +1,40 @@
+
+const stream2 = new Entity()
+stream2.addComponent(new GLTFShape('models/baseDarkWithCollider.glb'))
+stream2.addComponent(new Transform({ scale: new Vector3(1, 1, 0) }))
+engine.addEntity(stream2)
+
+const screenStream2 = new Entity()
+screenStream2.addComponent(new Transform({ position: new Vector3(-27.66, 1.99, 43), rotation: Quaternion.Euler(0, 91.10, 0) }))
+engine.addEntity(screenStream2)
+
+const screenTransform2 = new Entity()
+screenTransform2.addComponent(new Transform({ position: new Vector3(0, 1.99, 0) }))
+screenTransform2.getComponent(Transform).rotate(Vector3.Right(), 0)
+screenTransform2.setParent(screenStream2)
+
+const screen2 = new Entity()
+screen2.addComponent(new PlaneShape())
+screen2.addComponent(new Transform({ scale: new Vector3(14.02, 6.60, 6.60) }))
+screen2.getComponent(Transform).rotate(Vector3.Up(), 0)
+screen2.setParent(screenTransform2)
+screenTransform2.getComponent(Transform).scale.setAll(0.6) // You can change the scale of the screen here...
+
+const videoClip = new VideoClip(
+    'https://player.vimeo.com/external/722840634.m3u8?s=4b5289e586153f7bffcf1ec27f93a3e12f7348c8'
+)
+const videoTexture = new VideoTexture(videoClip)
+videoTexture.play()
+videoTexture.loop = true
+videoTexture.volume = 0.5
+
+// Adjust screen material to increase the brightness and clarity
+const screenMaterial = new Material()
+screenMaterial.albedoTexture = videoTexture
+screenMaterial.emissiveTexture = videoTexture
+screenMaterial.emissiveColor = Color3.White()
+screenMaterial.emissiveIntensity = 0.6
+screenMaterial.roughness = 1.0
+screen2.addComponent(screenMaterial)
+
+export = stream2;
