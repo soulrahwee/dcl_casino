ee22f8fa6d4e524663edd9f1c155016f6ddc285f
MineaChhem <chhemminea@gmail.com>
2022-06-29 09:34:56 +0700

    Testing Avatar

---
+++ src/npc&sound/avatar.ts
@@ -0,0 +1,7 @@
+import { NPC } from "@dcl/npc-scene-utils";
+
+const avatar = new NPC({position:new Vector3(1, 0, 1), scale: new Vector3(1, 1, 1)},'models/mantaRay.glb',              
+()=>{
+
+})
+avatar.addComponent(new Transform())
